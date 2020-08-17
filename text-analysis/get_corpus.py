cc5edd2be2a1238d73fd0a53c3129180673df1b5
Amith Lukkoor <amithlukkoor@gmail.com>
2020-08-17 16:59:52 -0700

    Json titling

--- text-analysis/get_corpus.py
+++ text-analysis/get_corpus.py
@@ -11,16 +11,16 @@ def scrape_corpus(ser):
         corpus += el.get_text()
     return corpus
 
-def write_top_json(articles):
+def write_top_json(articles, tag):
     top_articles = articles.loc[articles['claps'] >= 500].copy()
     top_articles['corpus'] = top_articles['url'].apply(scrape_corpus)
-    with open('top_health_articles.json', 'w') as f:
+    with open('top_' + tag + '_articles.json', 'w') as f:
         f.write(top_articles.to_json())
 
 start = time.time()
 dir = '../scraping/scraped_data/'
 file = '20180101health20200701.json'
 articles = pd.read_json(dir+file)
-write_top_json(articles)
+write_top_json(articles, 'health')
 end = time.time()
 print(end-start)
