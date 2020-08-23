9b70b1cae1e0ac5d1757b39dfc71fd20e3ebf0f2
f-ardila <fardila@ucsc.edu>
2020-08-23 08:58:23 -0700

    renamed output files which were previously just being named with the tag health

--- scraping/scrapy_wrapper.py
+++ scraping/scrapy_wrapper.py
@@ -7,9 +7,8 @@ start_date = '20180101'
 end_date = '20200701'
 tag = 'health'
 output_dir = 'scraped_data/'
-output_file = output_dir+start_date+'_'+tag+'_'+end_date+'.json'
 scrapper_file = 'medium_scraper_tag_archive.py'
-log_file = 'logs/'+start_date+'_'+tag+'_'+end_date+'_2.log'
+
 
 if os.path.exists(output_file):
     os.remove(output_file)
@@ -20,6 +19,10 @@ parser.add_argument('-tag', default=tag)
 parser.add_argument('-start_date', default=start_date)
 parser.add_argument('-end_date', default=end_date)
 args = parser.parse_args()
+
+#output files
+log_file = 'logs/'+start_date+'_'+args.tag+'_'+end_date+'.log'
+output_file = output_dir+start_date+'_'+args.tag+'_'+end_date+'.json'
 
 print('START DATE: '+args.start_date)
 print('END DATE: '+args.end_date)
