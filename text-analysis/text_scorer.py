975fe76347877bd0416704b50e2bca03f0911c62
Amith Lukkoor <amithlukkoor@gmail.com>
2020-09-17 18:39:11 -0700

    Text scorer

---
+++ text-analysis/text_scorer.py
@@ -0,0 +1,69 @@
+import sys
+import string
+import pandas as pd
+
+# Clean text
+def clean(text):
+    text = text.lower()
+    for char in ['\n', '“', '”']:
+        text = text.replace(char, '')
+    text = text.rstrip('written bywritten by')
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+# Count instances of each word
+def counter_df(text):
+    wordlist = text.split()
+    wordfreq = {}
+    for w in wordlist:
+        wordfreq[w] = wordlist.count(w)
+    counter = pd.DataFrame.from_dict(wordfreq, orient='index', columns = ['count'])
+    counter.index.name = 'word'
+    return counter.reset_index()
+    
+# Color to word database
+color_db = {
+    'purple_negative': ['mystery', 'mysterious', 'moodiness', 'moody', 'boredom', 'bored', 'bore', 'confusion', 'confuse', 'confused', 'disconnection', 'disconnect'],
+    'purple_positive': ['connection', 'connect', 'wisdom', 'wise', 'spirituality', 'spiritual', 'royalty', 'royal, ''nobility', 'noble', 'luxury', 'luxurious', 'ambition', 'ambitious', 'wealth', 'wealthy', 'awaken', 'awake'],
+    'blue_negative': ['coldness', 'cold', 'masculinity', 'masculine', 'male', 'disgust', 'disgusted', 'conflict', 'conflicting', 'aggression', 'aggressive'],
+    'blue_positive': ['intuition', 'imagination', 'imagine', 'tranquility', 'tranquil', 'security', 'secure', 'integrity', 'peace', 'peaceful', 'loyalty', 'loyal', 'faith', 'faithful', 'intelligence', 'intelligent'],
+    'teal_negative': ['femininity', 'feminine', 'female', 'hostility', 'hostile'],
+    'teal_positive': ['communication', 'communicate', 'expression', 'express', 'healing', 'heal', 'protection', 'protect', 'sophisticated', 'cleanse', 'cleansing'],
+    'green_negative': ['envy', 'envious', 'jealousy', 'jealous', 'guilt', 'guilty', 'fear', 'fearful', 'scared', 'judgmental', 'judge', 'judging', 'unforgiving', 'anxiety', 'anxious'],
+    'green_positive': ['compassion', 'trust', 'freshness', 'fresh', 'environment', 'new', 'money', 'fertile', 'health', 'healthy', 'grounded', 'reconnecting', 'balanced', 'balance', 'balancing'],
+    'yellow_negative': ['irresponsible', 'instability', 'grief', 'grieve', 'grieving', 'grieves', 'addiction', 'addict', 'addicted', 'insecurity', 'insecure', 'depression', 'depressed'],
+    'yellow_positive': ['confident', 'confidence', 'bright', 'sunny', 'energetic', 'warm', 'happy', 'happiness', 'perky', 'joy', 'joyful', 'intellect', 'intellectual'],
+    'orange_negative': ['ignorance', 'ignorant', 'sluggishness', 'sluggish', 'shame', 'ashamed', 'shameful', 'compulsiveness', 'compulsive', 'loneliness', 'lonely', 'alone', 'dependence', 'dependent'],
+    'orange_positive': ['courage', 'friendliness', 'friendly', 'success', 'successful', 'creativity', 'creative', 'openness', 'open', 'sexual', 'sexy', 'sex'],
+    'red_negative': ['anger', 'angry', 'unsafe', 'warned', 'warn', 'warning', 'worry', 'worried', 'volatile', 'hopelessness', 'hopeless'],
+    'red_positive': ['love', 'loving', 'passion', 'passionate', 'energy', 'energetic', 'power', 'powerful', 'strength', 'strong', 'heat', 'hot', 'desire', 'safe', 'safety', 'instinctive', 'instinct', 'security', 'secure', 'liberating', 'liberate']
+}
+
+# Find color for word
+def find_color(word):
+    for key in color_db.keys():
+        if word in color_db[key]:
+            return key
+    return ''
+           
+def main():
+    # Read file
+    in_file = str(sys.argv[1])
+    with open(in_file, 'r') as f:
+        all_lines = f.read()
+
+    # Clean text
+    clean_corpus = clean(all_lines)
+
+    # Count words
+    data_df = counter_df(clean_corpus)
+
+    # Count colors
+    data_df['color'] = data_df['word'].apply(find_color)
+    color_df = data_df.groupby('color').sum().drop([''])
+
+    # Return results
+    print(color_df)
+
+if __name__ == "__main__":
+    main()
