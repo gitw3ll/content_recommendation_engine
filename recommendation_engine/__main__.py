e3eca2ca851ff2fbaafee8785246ce59adba6a34
Chingyi Ie <iechingyi@yahoo.com>
2020-12-11 18:49:42 -0800

    updated recommender model

--- recommendation_engine/__main__.py
+++ recommendation_engine/__main__.py
@@ -1,9 +1,9 @@
 from recommender_model import RecommenderModel
 
-DATA_DIRECTORY = 'data/'
+DATA_DIRECTORY = 'recommendation_engine/data/'
 
 rec = RecommenderModel()
 rec.load_train_data(DATA_DIRECTORY)
 rec.train()
 pred = rec.predict(user_index=50)
-print(f'predictions: {pred}')
+print(pred)
--- recommendation_engine/recommender_model.py
+++ recommendation_engine/recommender_model.py
@@ -17,13 +17,14 @@ class RecommenderModel:
         self.articles_df = None
         self.interactions_df = None
         self.model = None
+        self.sparse_matrix = None
         self.train_data = None
 
     def load_train_data(self, data_directory):
         """
         """
         self.articles_df = pd.read_csv(os.path.join(data_directory, 'shared_articles.csv'))
-        self.interactions_df = pd.read_csv(os.path.join(data_directory, 'user_interactoins.csv'))
+        self.interactions_df = pd.read_csv(os.path.join(data_directory, 'users_interactions.csv'))
     
     def preprocess_data(self, k1=1.2, b=0.75):
         """
@@ -47,9 +48,14 @@ class RecommenderModel:
         df['person_id'] = df['personId'].cat.codes
         df['content_id'] = df['contentId'].cat.codes
 
+        # Converting to coordinate format to increase space efficiency
         sparse_content_person = sparse.coo_matrix((df['eventStrength'].astype(float), (df['content_id'], df['person_id'])))
+        #print(f'Before bm25: {sparse_content_person}')
         sparse_content_person = bm25_weight(sparse_content_person, K1=k1, B=b)
-        self.train_data = sparse_content_person.tocsr()
+        #print(f'After bm25: {sparse_content_person}')
+
+        self.train_data = df
+        self.sparse_matrix = sparse_content_person.tocsr()
 
     def setup_model(self, factors=64, regularization=0.001, use_native=True, use_cg=True):
         """
@@ -69,13 +75,18 @@ class RecommenderModel:
         self.preprocess_data(k1=100, b=0.8)
 
         self.model = self.setup_model()
-        self.model.fit(self.train_data)
+        self.model.fit(self.sparse_matrix)
     
     def predict(self, user_index, n_predict=10):
         """
         """
-        user_data = self.train_data.T.tocsr()
-        return self.model.recommend(user_index, user_data, N=n_predict)
+        user_data = self.sparse_matrix.T.tocsr()
+        recommendations = self.model.recommend(user_index, user_data, N=n_predict)
+
+        item_indices, scores = map(list, zip(*recommendations))
+        titles = [self.train_data['title'][index] for index in item_indices]
+
+        return dict(zip(titles, scores))
 
 
 
