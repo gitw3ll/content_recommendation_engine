5fc6aae8a17ecc13bc08412512461f9769225424
VikramO <vikramoruganti@gmail.com>
2020-12-29 04:07:10 -0500

    Update rec_database_schema.sql

--- recommendation_engine/rec_engine_database/rec_database_schema.sql
+++ recommendation_engine/rec_engine_database/rec_database_schema.sql
@@ -1,4 +1,6 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS content_data;
+DROP TABLE IF EXISTS user_data;
 CREATE TABLE content_data (content_id int, title text);
 CREATE TABLE user_data (person_id int, content_id int, title text);
 COMMIT;
