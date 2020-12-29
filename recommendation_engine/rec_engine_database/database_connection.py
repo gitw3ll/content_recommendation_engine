a79b85fd4e3e9970e5866a6728a1d5ad10b6d119
VikramO <vikramoruganti@gmail.com>
2020-12-29 03:56:03 -0500

    Update database_connection.py
    Added comment above code

--- recommendation_engine/rec_engine_database/database_connection.py
+++ recommendation_engine/rec_engine_database/database_connection.py
@@ -1,6 +1,9 @@
+#Same as Ethan's code in database.py 
+#database.py is in the webapp directory
+
 import sqlite3
 
-def database_connection(filepath):
+def get_db_connection(filepath):
     conn = sqlite3.connect(filepath)
     conn.row_factory = sqlite3.Row
     return conn
