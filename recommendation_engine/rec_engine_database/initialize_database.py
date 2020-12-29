4d950e517da8a62e931baf42620cf9099ae5ffdd
VikramO <vikramoruganti@gmail.com>
2020-12-29 04:32:05 -0500

    Update initialize_database.py
    Added comments above code

--- recommendation_engine/rec_engine_database/initialize_database.py
+++ recommendation_engine/rec_engine_database/initialize_database.py
@@ -1,8 +1,11 @@
+#Same as Ethan's code in init_db.py
+#Same notice; running this with existing database will erase the existing database
+
 import sqlite3
 
 connection = sqlite3.connect('user_content_data.db')
 
-with open(r'C:/Users/Vikram/rec_database_schema.sql') as f:
+with open('../rec_database_schema.sql') as f:
     connection.executescript(f.read())
 
 connection.commit()
