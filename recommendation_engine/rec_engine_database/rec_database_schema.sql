dc75387ad80b7b32e6d053accad8a94e534f96aa
VikramO <vikramoruganti@gmail.com>
2020-12-30 20:57:58 -0500

    Update rec_database_schema.sql
    Removed title column from user_data

--- recommendation_engine/rec_engine_database/rec_database_schema.sql
+++ recommendation_engine/rec_engine_database/rec_database_schema.sql
@@ -2,5 +2,5 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS content_data;
 DROP TABLE IF EXISTS user_data;
 CREATE TABLE content_data (content_id int, title text);
-CREATE TABLE user_data (person_id int, event_type text, content_id int, title text);
+CREATE TABLE user_data (person_id int, event_type text, content_id int);
 COMMIT;
