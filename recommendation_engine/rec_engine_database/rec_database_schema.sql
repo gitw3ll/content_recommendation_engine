8624beacf470ebd5ad96313430b34c426c2672d9
VikramO <vikramoruganti@gmail.com>
2021-01-03 11:25:04 -0500

    Update rec_database_schema.sql
    Added user wellness scores table

--- recommendation_engine/rec_engine_database/rec_database_schema.sql
+++ recommendation_engine/rec_engine_database/rec_database_schema.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS scores_data;
 DROP TABLE IF EXISTS content_data;
 DROP TABLE IF EXISTS user_data;
+CREATE TABLE scores_data (person_id int, mental int, emotional int, diet int, wq int);
 CREATE TABLE content_data (content_id int, title text);
 CREATE TABLE user_data (person_id int, event_type text, content_id int);
 COMMIT;
